ac0b13553eb44aa2f1d1742e8870445cf36a11c0
MalavikaAjit <malavikaajit9@gmail.com>
2021-04-23 00:07:26 +0530

    ADD. added feature to add contact

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     //Scanner scan = new Scanner(System.in);
     public ArrayList<Contact> contactList = new ArrayList<>();
-
+    //adding contacts
     public void addContact(Scanner scan) {
         System.out.println("Add Details ");
         System.out.println("Enter First Name");
@@ -27,82 +27,7 @@ public class Main {
         contactList.add(contact);
         System.out.println("Contact added Successfully");
     }
-    public void editContact(Scanner scan) {
-        System.out.println("Enter contact name to edit ");
-        String contactName = scan.next();
-        boolean found = false;
-        for (Contact person : contactList) {
-            System.out.println(person.toString());
-            if (contactName.equalsIgnoreCase(person.firstName)) {
-                found=true;
-
-                System.out.println("Enter choice to edit\n"
-                        + "1.Edit last name\n"
-                        + "2. Edit email\n"
-                        + "3.Edit Mobile No.\n"
-                        + "4.Edit State\n"
-                        + "5.Edit city\n"
-                        + "6.Edit zip code\n"
-                        + "7.Edit address\n");
-                int choice2 = scan.nextInt();
-                scan.nextLine();
-                //------------------
-                switch (choice2) {
-                    case 1: {
-                        System.out.println("Enter Last Name \n");
-                        String last = scan.nextLine();
-                        person.setLastName(last);
-                        break;
-                    }
-                    case 2: {
-                        System.out.println("Enter email");
-                        String email = scan.nextLine();
-                        person.setEmail(email);
-                        break;
-                    }
-                    case 3: {
-                        System.out.println("Enter Mobile no");
-                        long mobileNo = scan.nextLong();
-                        //Use nextLine() even after taking long input
-                        scan.nextLine();
-                        person.setMobileNo(mobileNo);
-                        break;
-                    }
-                    case 4: {
-                        System.out.println("Enter State");
-                        String state = scan.nextLine();
-                        person.setState(state);
-                        break;
-                    }
-                    case 5: {
-                        System.out.println("Enter city");
-                        String city = scan.nextLine();
-                        person.setCity(city);
-                        break;
-                    }
-                    case 6: {
-                        System.out.println("Enter zip code");
-                        int zip = scan.nextInt();
-                        scan.nextLine();
-                        person.setZip(zip);
-                        break;
-                    }
-                    case 7: {
-                        System.out.println("Enter Address");
-                        String address = scan.nextLine();
-                        person.setCity(address);
-                        break;
-                    }
-                    default:System.out.println("exit");
-                }
-
-            }
-            if (found)
-                break;
-        }
-        if (!found)
-            System.out.println("No such contact");
-    }
+    //displaying contacts
     public void displayContact() {
         System.out.println("Contact Details :");
         for (Contact person : contactList) {
@@ -116,13 +41,11 @@ public class Main {
 
         boolean loop=true;
         while (loop) {
-
             System.out.println("Enter an option");
             System.out.println("Options are\n"
                     + "1.Add contact\n"
-                    + "2.Edit contact\n"
-                    + "3.Display contact\n"
-                    + "4.Exit\n");
+                    + "2.Display contact\n"
+                    + "3.Exit\n");
             int choice = scan.nextInt();
             scan.nextLine();
             switch (choice) {
@@ -130,12 +53,9 @@ public class Main {
                     main.addContact(scan);
                     break;
                 case 2:
-                    main.editContact(scan);
-                    break;
-                case 3:
                     main.displayContact();
                     break;
-                case 4:
+                case 3:
                     System.out.println("Exit");
                     loop = false;
                     break;
